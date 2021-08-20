e5acc9c247141a63f4b4d9d680cb7e91de9127c6
maplethorpej <jeradmaplethorpe@gmail.com>
2021-08-19 20:41:51 -0500

    clean up nav and add donate, feedback and logout options

--- src/app/components/InventorySidebar/index.tsx
+++ src/app/components/InventorySidebar/index.tsx
@@ -1,32 +1,32 @@
-import * as React from "react";
-import { Menu, Dropdown } from "antd";
+import * as React from "react"
+import { Menu, Dropdown } from "antd"
 import {
   CaretDownFilled,
   CheckCircleFilled,
   PlusOutlined,
-} from "@ant-design/icons";
+} from "@ant-design/icons"
 
-import { Item } from "types/item";
-import { Category } from "types/category";
+import { Item } from "types/item"
+import { Category } from "types/category"
 
-import { getCategories } from "lib/utils/categories";
-import { searchItems } from "lib/utils/search";
+import { getCategories } from "lib/utils/categories"
+import { searchItems } from "lib/utils/search"
 
-import { CategoryGroup, InventoryContainer } from "styles/common";
+import { CategoryGroup, InventoryContainer } from "styles/common"
 import {
   InventoryItem,
   CategoryLabel,
   ItemContainer,
   CategoryHeader,
-} from "./styles";
-import { Input } from "../FormFields";
-import FloatingActionButton from "../FloatingActionButton";
+} from "./styles"
+import { Input } from "../FormFields"
+import FloatingActionButton from "../FloatingActionButton"
 interface SidebarProps {
-  items: Item[];
-  addItem: (item: Item) => void;
-  removeItem: (id: number) => void;
-  createNewItem: () => void;
-  currentItems: number[];
+  items: Item[]
+  addItem: (item: Item) => void
+  removeItem: (id: number) => void
+  createNewItem: () => void
+  currentItems: number[]
 }
 
 const InventorySidebar: React.FC<SidebarProps> = ({
@@ -36,34 +36,34 @@ const InventorySidebar: React.FC<SidebarProps> = ({
   currentItems,
   createNewItem,
 }) => {
-  const categories = getCategories(items);
-  const [filterText, setFilterText] = React.useState<string>("");
+  const categories = getCategories(items)
+  const [filterText, setFilterText] = React.useState<string>("")
   const [categoryFilter, setCategoryFilter] = React.useState<Category | null>(
     null
-  );
+  )
 
   const handleAddItem = (id: number) => {
-    const item = items.find((item) => item.id === id);
+    const item = items.find((item) => item.id === id)
     if (!item) {
-      return;
+      return
     }
-    addItem(item);
-  };
+    addItem(item)
+  }
 
   const renderCategoryItems = (catItems: Item[]) =>
     catItems.map((item) => {
-      const { id, name, product_name } = item;
-      const isSelected = currentItems.includes(id);
+      const { id, name, product_name } = item
+      const isSelected = currentItems.includes(id)
 
       const handleSelect = () => {
         if (isSelected) {
-          removeItem(id);
+          removeItem(id)
         } else {
-          handleAddItem(id);
+          handleAddItem(id)
         }
-      };
+      }
 
-      const status = isSelected ? <CheckCircleFilled /> : null;
+      const status = isSelected ? <CheckCircleFilled /> : null
       return (
         <InventoryItem
           key={id}
@@ -78,19 +78,19 @@ const InventorySidebar: React.FC<SidebarProps> = ({
             <div>{status}</div>
           </ItemContainer>
         </InventoryItem>
-      );
-    });
+      )
+    })
 
   const renderInventory = () => {
-    const cats = categoryFilter ? [categoryFilter] : categories;
+    const cats = categoryFilter ? [categoryFilter] : categories
     return cats.map((category) => {
-      const categoryItems = items.filter((i) => i.categoryId === category.id);
+      const categoryItems = items.filter((i) => i.categoryId === category.id)
       const filteredItem = !!filterText
         ? searchItems(categoryItems, filterText)
-        : categoryItems;
+        : categoryItems
 
       if (!filteredItem.length) {
-        return null;
+        return null
       }
 
       return (
@@ -100,16 +100,16 @@ const InventorySidebar: React.FC<SidebarProps> = ({
           </CategoryHeader>
           {renderCategoryItems(filteredItem)}
         </CategoryGroup>
-      );
-    });
-  };
+      )
+    })
+  }
 
   const categoryMenu = () => {
     const categoryOptions = categories.map((cat) => (
       <Menu.Item key={cat.id} onClick={() => setCategoryFilter(cat)}>
         {cat.name}
       </Menu.Item>
-    ));
+    ))
     return (
       <Menu>
         <Menu.Item key={-1} onClick={() => setCategoryFilter(null)}>
@@ -117,10 +117,10 @@ const InventorySidebar: React.FC<SidebarProps> = ({
         </Menu.Item>
         {categoryOptions}
       </Menu>
-    );
-  };
+    )
+  }
 
-  const filterLabel = categoryFilter ? categoryFilter.name : "All Categories";
+  const filterLabel = categoryFilter ? categoryFilter.name : "All Categories"
   const categoryDropdown = () => (
     <Dropdown overlay={categoryMenu()} trigger={["click"]}>
       <CategoryLabel>
@@ -128,7 +128,7 @@ const InventorySidebar: React.FC<SidebarProps> = ({
         <CaretDownFilled />
       </CategoryLabel>
     </Dropdown>
-  );
+  )
 
   return (
     <>
@@ -148,16 +148,16 @@ const InventorySidebar: React.FC<SidebarProps> = ({
       ></FloatingActionButton>
       <InventoryContainer>{renderInventory()}</InventoryContainer>
     </>
-  );
-};
+  )
+}
 
 function isEqual(prev: SidebarProps, next: SidebarProps) {
-  const { currentItems: prevItems, items: prevInventory } = prev;
-  const { currentItems: nextItems, items: nextInventory } = next;
+  const { currentItems: prevItems, items: prevInventory } = prev
+  const { currentItems: nextItems, items: nextInventory } = next
   return (
     prevItems.length === nextItems.length &&
     prevInventory.length === nextInventory.length
-  );
+  )
 }
 
-export default React.memo(InventorySidebar, isEqual);
+export default React.memo(InventorySidebar, isEqual)
