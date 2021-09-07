9e4ad4746e44035b7782d6f94a9014187f9d591a
maplethorpej <jeradmaplethorpe@gmail.com>
2021-09-06 21:10:37 -0500

    install chakra-ui

--- src/index.tsx
+++ src/index.tsx
@@ -1,40 +1,44 @@
-import ReactDOM from 'react-dom';
-import * as Sentry from '@sentry/browser';
-import { ThemeProvider } from "styled-components";
-import DocumentTitle from 'react-document-title';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import ReactDOM from "react-dom"
+import * as Sentry from "@sentry/browser"
+import { ThemeProvider } from "styled-components"
+import DocumentTitle from "react-document-title"
+import { BrowserRouter as Router, Route } from "react-router-dom"
 
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import { AppProvider } from './AppContext';
-import WithAnalytics from 'app/components/higher-order/with-analytics';
-import { HistoryListener } from 'react-router-navigation-confirm';
+import App from "./App"
+import * as serviceWorker from "./serviceWorker"
+import { AppProvider } from "./AppContext"
+import WithAnalytics from "app/components/higher-order/with-analytics"
+import { ChakraProvider } from "@chakra-ui/react"
+import { HistoryListener } from "react-router-navigation-confirm"
 
-import { theme } from 'styles/theme';
-import 'antd/dist/antd.css'
-import 'styles/style.css';
+import { theme } from "styles/theme"
+import "antd/dist/antd.css"
+import "styles/style.css"
 
-Sentry.init({ dsn: process.env.REACT_APP_SENTRY_DNS });
+Sentry.init({ dsn: process.env.REACT_APP_SENTRY_DNS })
 
 ReactDOM.render(
-    <AppProvider>
-        <ThemeProvider theme={theme}>
-            <DocumentTitle title={`Packstack | Backpacking packing lists`}>
-                <Router>
-                    <Route render={(props) => (
-                        <WithAnalytics {...props}>
-                            <HistoryListener>
-                                <App/>
-                            </HistoryListener>
-                        </WithAnalytics>
-                    )}/>
-                </Router>
-            </DocumentTitle>
-        </ThemeProvider>
-    </AppProvider>,
-    document.getElementById('root'));
+  <AppProvider>
+    <ThemeProvider theme={theme}>
+      <DocumentTitle title={`Packstack | Backpacking packing lists`}>
+        <Router>
+          <Route
+            render={(props) => (
+              <WithAnalytics {...props}>
+                <HistoryListener>
+                  <App />
+                </HistoryListener>
+              </WithAnalytics>
+            )}
+          />
+        </Router>
+      </DocumentTitle>
+    </ThemeProvider>
+  </AppProvider>,
+  document.getElementById("root")
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
